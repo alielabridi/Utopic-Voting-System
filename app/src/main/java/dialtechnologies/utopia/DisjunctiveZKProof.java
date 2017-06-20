f15032c7c64bc035eea2ed04666bb26b8b6d5766
alielabridi <elabridiali@outlook.com>
2017-06-20 12:43:43 +0000

    Disjunctive ZKProof

---
+++ app/src/main/java/dialtechnologies/utopia/DisjunctiveZKProof.java
@@ -0,0 +1,12 @@
+package dialtechnologies.utopia;
+
+/**
+ * Created by Ali on 6/20/2017.
+ */
+public class DisjunctiveZKProof {
+    // A DisjunctiveZKProof is a sequence of ZKProofs for values Min through Max
+    // (usually corresponding to Question.Min and Question.Max). Only one of the
+    // values is a real ZKProof; the others are simulated. It is constructed using
+    // the Fiat-Shamir heuristic as described in the comment for ZKProof.
+    ZKProof[] DisjunctiveZKProof;
+}
