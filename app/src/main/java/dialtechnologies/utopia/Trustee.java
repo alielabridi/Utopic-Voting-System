6259fb70e06ca707874ce3eb1e082d682cded0de
alielabridi <elabridiali@outlook.com>
2017-06-20 16:23:08 +0000

    Trustee Class

---
+++ app/src/main/java/dialtechnologies/utopia/Trustee.java
@@ -0,0 +1,44 @@
+package dialtechnologies.utopia;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.math.BigInteger;
+
+/**
+ * Created by Ali Elabridi on 6/20/2017.
+ * all the data structures above was extracted from the original structure of Helios
+ * and added some convenience data
+ */
+
+public class Trustee {
+    // A Trustee represents the public information for one of the keys used to
+    // tally and decrypt the election results.
+
+    // DecryptionFactors are the partial decryptions of each of the
+    // homomorphic tally results.
+    @JsonProperty("decryption_factors")
+    BigInteger[][] DecryptionFactors;
+
+    // DecryptionProofs are the proofs of correct partial decryption for
+    // each of the DecryptionFactors.
+    @JsonProperty("decryption_proofs")
+    ZKProof[][] DecryptionProofs;
+
+    // PoK is a proof of knowledge of the private key share held by this
+    // Trustee and used to create the DecryptionFactors.
+    @JsonProperty("pok")
+    SchnorrProof PoK;
+
+    // PublicKey is the ElGamal public key of this Trustee.
+    @JsonProperty("public_key")
+    Key PublicKey;
+
+    // PublicKeyHash is the SHA-256 hash of the JSON representation of
+    // PublicKey.
+    @JsonProperty("public_key_hash")
+    String PublicKeyHash;
+
+    // Uuid is the unique identifier for this Trustee.
+    @JsonProperty("uuid")
+    String Uuid;
+}
